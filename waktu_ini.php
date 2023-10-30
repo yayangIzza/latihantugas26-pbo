e47bdf01c146be202cff3f805da3c6da59c39348
yayangIzza <149374422+yayangIzza@users.noreply.github.com>
2023-10-30 19:53:29 +0700

    Add files via upload

---
+++ waktu_ini.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Nama  : Yayang Izzatul Haq<br>";
+echo "NIM   : 22205052<br>";
+echo "Kelas : PBO<br>";
+
+
+date_default_timezone_set('Asia/Jakarta');
+
+echo "<br/>";
+echo "hari ini adalah hari =".date('l, d M Y  H:i:s ');
+
+?>
